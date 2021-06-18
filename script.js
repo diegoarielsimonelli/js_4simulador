137c31aee8ab83a899c70478ea267e0a837908a2
diegoarielsimonelli <simonellidiego@gmail.com>
2021-06-18 20:04:36 -0300

    2

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
-// function solicitarNombre(){
-//     let nombre= prompt("Bievenido/a, ingrese su nombre");
-//     alert("Bienvenido/a"+" "+ nombre+" "+ "encontrarás productos de calidad, esperamos que los disfrutes. Tenes un 20% off en la primer compra");
-// }
-// var nombre1 =solicitarNombre();
+function solicitarNombre(){
+    let nombre= prompt("Bievenido/a, ingrese su nombre");
+    alert("Bienvenido/a"+" "+ nombre+" "+ "encontrarás productos de calidad, esperamos que los disfrutes. Tenes un 20% off en la primer compra");
+}
+solicitarNombre();
 
-// let producto=parseInt(prompt( "¿qué producto te gustaria comprar? 1- Miel 2- Ghee 3- Palta 4-aceite de oliva 5- No quiero comprar" )) ;
-// let compra=0
+let producto=parseInt(prompt( "¿qué producto te gustaria comprar? 1- Miel 2- Ghee 3- Palta 4-aceite de oliva 5- No quiero comprar" )) ;
+let compra=0
 
     
-// switch(producto){
-//     case 1:
-//         compra += 40
-//         alert("la miel tiene un valor de $40");
+switch(producto){
+    case 1:
+        compra += 40
+        alert("la miel tiene un valor de $40");
         
-//     case 2:
-//         compra += 50
-//         alert("Ghee tiene un valor de $50");
+    case 2:
+        compra += 50
+        alert("Ghee tiene un valor de $50");
         
-//     case 3:
-//         compra += 60
-//         alert("La palta tiene un valor de $60");
+    case 3:
+        compra += 60
+        alert("La palta tiene un valor de $60");
         
-//     case 4:
-//         compra += 100
-//         alert("El aceite de oliva tiene un valor de $100");
+    case 4:
+        compra += 100
+        alert("El aceite de oliva tiene un valor de $100");
         
-//     case 5:
-//         alert("Gracias, vuelva pronto")
-//         break;
-// }
+    case 5:
+        alert("Gracias, vuelva pronto")
+        break;
+}
  
-// function descuento(compra){
-//     let desc= compra * 0.20;
-//     alert("el 20% off de la compra es $" + desc);
-//     let resta= compra - desc
-//     alert("solo resta abonar $ "+ resta);
+function descuento(compra){
+    let desc= compra * 0.20;
+    alert("el 20% off de la compra es $" + desc);
+    let resta= compra - desc
+    alert("solo resta abonar $ "+ resta);
 
-// }
-// descuento(compra)
+}
+descuento(compra);
 
